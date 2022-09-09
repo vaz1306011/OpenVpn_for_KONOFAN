6f9310db129ac8e5a2f2c464280d8df9ba346d0d
vaz1306011 <vaz1306011@gmail.com>
2022-09-09 17:26:51 +0800

    稍微整理一下

--- OpenVpn_for_KONOSUBA.py
+++ OpenVpn_for_KONOSUBA.py
@@ -5,16 +5,19 @@ import requests
 from bs4 import BeautifulSoup
 
 if __name__ == "__main__":
-    target_country = "Japan"
-    black_list = ["219.100."]
     n = int(input("輸入編號:"))
 
     URL = "https://www.vpngate.net/"
-    vpngate = requests.get(URL + "cn/")
+    URL_CN = "https://www.vpngate.net/cn/"
+    vpngate = requests.get(URL_CN)
     soup = BeautifulSoup(vpngate.text, "html.parser")
+    config = configparser.ConfigParser()
+    config.read("config.ini", encoding="utf-8")
 
     count = 0
     rows = soup.select("#vpngate_main_table")[0].find_all("tr")[17:]
+    target_country = "Japan"
+    black_list = ["219.100."]
     for row in rows:
         tds = row.find_all("td")
 
@@ -28,7 +31,7 @@ if __name__ == "__main__":
 
         # 檢查黑名單
         ip = tds[1].find_all("span")[1].text
-        if all([ip.startswith(i) for i in black_list]):
+        if any([ip.startswith(i) for i in black_list]):
             continue
 
         count += 1
@@ -38,31 +41,29 @@ if __name__ == "__main__":
         print("沒有符合的ip")
         exit()
 
-    download_url = URL + "cn/" + tds[6].a["href"]
+    download_url = URL_CN + tds[6].a["href"]
     openvpn = requests.get(download_url)
     soup = BeautifulSoup(openvpn.text, "html.parser")
     ovpn = soup.select("ul.listBigArrow li a")
 
     def get_url(ovpn):
         for t in ovpn:
-            if "TCP" in str(t.strong):
+            if "TCP" in str(t.text):
                 return URL + t["href"]
 
         for t in ovpn:
-            if "UDP" in str(t.strong):
+            if "UDP" in str(t.text):
                 return URL + t["href"]
 
     file = requests.get(get_url(ovpn))
-    config = configparser.ConfigParser()
-    config.read("address.ini", encoding="utf-8")
-    address = config["address"]["address"]
-    with open(address, "wb") as fp:
+    path = config.get("config", "path")
+    with open(path, "wb") as fp:
         fp.write(file.content)
         fp.write(
-            "route-nopull\ndhcp-option DNS 8.8.8.8\nroute api.konosubafd.jp\nroute static.konosubafd.jp".encode()
+            "route-nopull\ndhcp-option DNS 8.8.8.8\nroute api.konosubafd.jp\nroute static.konosubafd.jp\n".encode()
         )
 
     print(f"IP:{ip}")
-    print(f"已輸出至 {address}")
+    print(f"已輸出至 {path}")
 
     os.system("pause")
